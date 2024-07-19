745d9910b85badee7f8405c7d06be9fbd8dadf37
Devi Sri Sai Charan <devisrisaicharan@gmail.com>
2024-07-19 12:03:10 +0530

    Pivot Element (correct solution)

--- C++/Arrays/Week_4/PivotElement.cpp
+++ C++/Arrays/Week_4/PivotElement.cpp
@@ -10,15 +10,19 @@ int PivotElement(vector<int> arr){
     while(left <= right){
         int mid = left + (right - left) / 2;
 
-        if(arr[mid] < arr[mid-1] && arr[mid] < arr[mid-1]) return mid-1;
-        else if(arr[mid] < arr[mid-1]) right = mid - 1;
-        else if(arr[mid] > arr[mid-1]) left = mid + 1;
+        if(left == right) return left;
+        
+        if(arr[mid] > arr[mid+1]) return mid;
+        else if(arr[mid] < arr[mid-1]) return mid-1;
+        else if(arr[mid] < arr[left]) right = mid-1;
+        else left = mid + 1;        
     }
     return -1;
 }
 
 int main(){
     //This is a rotated sorted array by 3
+    //Pivot Index --> Index of highest element
     vector<int> arr = {12,14,16,2,4,6,8,10};
 
     cout<<PivotElement(arr)<<endl;
