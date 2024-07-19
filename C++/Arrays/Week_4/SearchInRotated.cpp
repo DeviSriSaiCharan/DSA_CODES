7128cd7561ca2f7ef34732b0379459d3051383da
Devi Sri Sai Charan <devisrisaicharan@gmail.com>
2024-07-19 12:17:52 +0530

    Search in rotated array(DUMB VERSION)

---
+++ C++/Arrays/Week_4/SearchInRotated.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int PivotElement(vector<int> arr){
+    int n = arr.size();
+    int left = 0;
+    int right = n - 1;
+
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+
+        if(left == right) return left;
+        
+        if(arr[mid] > arr[mid+1]) return mid;
+        else if(arr[mid] < arr[mid-1]) return mid-1;
+        else if(arr[mid] < arr[left]) right = mid-1;
+        else left = mid + 1;        
+    }
+    return -1;
+}
+
+int BinarySearch(vector<int> arr, int left, int right, int target){
+    
+    while(left <= right){
+        int mid = (left+right)/2;
+
+        if(arr[mid] == target) return mid;
+        else if(arr[mid] > target) right = mid-1;
+        else left = mid + 1;
+    }
+    return -1;
+}
+
+//Search in rotated sorted array
+int SearchRotated(vector<int> arr, int target){
+    int n = arr.size();
+    int pivot = PivotElement(arr);
+
+    return max(BinarySearch(arr, 0, pivot, target), BinarySearch(arr, pivot+1, n-1, target));
+    
+}
+
+int main(){
+
+    vector<int> arr = {12,14,16,2,4,6,8,10};
+    int target = 10;
+
+    cout<<SearchRotated(arr, target);
+
+    return 0;
+}
